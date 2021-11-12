152d9a6ccbd9bf5f72347f06fb2bcc903bb24586
sgav191 <swgavin0@gmail.com>
2021-11-12 12:36:09 +0000

    Initial commit (finished)

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+#Imports
 import time
 import random
+from os import system
+#Introduction and Name Entry
+system('clear')
 print ("Hello and welcome to Noughts & Crosses!")
-time.sleep(1)
+input("Press enter when you are ready to continue...")
 player1name = input ("What is your name, Player 1: ")
 player2name = input ("What is your name, Player 2: ")
 print ("Let's see who is going to choose their type of piece!")
 input ("Press enter to find out...")
 print ("Choosing...")
 time.sleep(5)
+#Set Player Positions
 def ChooseSelectPlayer():
     players = ["Player 1","Player 2"]
     selectplayer = random.choice(players)
@@ -44,7 +49,9 @@ print (f"""Summary:
 {player1name} is {player1}
 {player2name} is {player2}
 """)
-input("Press enter when you're ready to start...")
+input("Press enter when you're ready to start the game...")
+system('clear')
+#Main Game & CheckWinnerFunction
 board = [" "," "," ",
          " "," "," ",
          " "," "," "]
@@ -55,4 +62,61 @@ def DrawBoard():
     print('-+-+-')
     print (board[6]+"|"+board[7]+"|"+board[8])
 
+def CheckWinner():
+    if board[0] == board[1] == board[2]:
+        return (True)
+    elif board[3] == board[4] == board[5]:
+        return (True)
+    elif board[6] == board[7] == board[8]:
+        return (True)
+    elif board[0] == board[3] == board[6]:
+        return (True)
+    elif board[1] == board[4] == board[7]:
+        return (True)
+    elif board[2] == board[5] == board[8]:
+        return (True)
+    elif board[0] == board[4] == board[8]:
+        return (True)
+    elif board[2] == board[4] == board[6]:
+        return (True)
+    else:
+        return (False)
+        
 DrawBoard()
+gameloop = True
+playergo = random.randint(1,2)
+
+while gameloop:
+    if playergo == 1:
+        player1go = input ("Player 1! It's your go, what is your move: ")
+        player1square = int(player1go)-1
+        if player1 == "Noughts":
+            board[player1square] = "O"
+        elif player1 == "Crosses":
+            board[player1square] = "X"
+        system('clear')
+        DrawBoard()
+    else:
+        player2go = input ("Player 2! It's your go whats your move: ")
+        player2square = int(player2go)-1
+        if player2 == "Noughts":
+            board[player2square] = "O"
+        elif player2 == "Crosses":
+            board[player2square] = "X"
+        system('clear')
+        DrawBoard()
+    checkwinner = CheckWinner()
+    if checkwinner == True:
+        gameloop = False
+    
+    if playergo == 1:
+        playergo = 2
+    else:
+        playergo = 2
+    
+winner = playergo
+if winner == 1:
+    winnername = player1name
+else:
+    winnername = player2name
+print (f"The winner is {winnername}! Thanks for playing!")
